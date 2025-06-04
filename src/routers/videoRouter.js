9a449ec47e34e7ff7754ab7e04ddc4c9c45bad17
LimitlessDevv <inyoungchoi26@gmail.com>
2025-06-04 22:35:12 +0900

    Extending Pug Templates

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import { trending, see, edit, search } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 // :id : url 경로의 일부를 변수처럼 받아들이겠다는 의미
 // (\d+) : 정규 표현식으로, 숫자만 허용 하겠다는 의미
 // \d는 숫자 하나를 의미 \d+는 숫자 하나 이상을 의미, Javascript에서는 \를 escape 해야 해서, \\d+라고 쓴다
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", trending);
 videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
+// videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+// videoRouter.get("/upload", upload);
 
 export default videoRouter;
