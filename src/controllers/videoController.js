96015f8e898ae0d853536c113521de616568b679
inyoung <inyoungchoi26@gmail.com>
2025-06-05 13:17:20 +0900

    using conditional

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,11 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
+const fakeUser = {
+	username: "Inyoung",
+	loggedIn : false
+};
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Hi", fakeUser });
+// { } 중괄호 안의 변수는 home 템플릿에 전달되는 객체
+
+export const see = (req, res) => res.render("watch")
+export const edit = (req, res) => res.render("edit")
+export const search = (req, res) => res.send("Search")
