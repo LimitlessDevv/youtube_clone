2334d3a591ec361e32e576d601a99d282007b1bd
inyoung <inyoungchoi26@gmail.com>
2025-07-01 11:15:31 +0900

    DB Array

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -25,17 +25,18 @@ let videos = [
   },
 ];
 
-export const trending = (req, res) =>{
-  return res.render("home", {pageTitle: "Home", videos})
-};
+export const trending = (req, res) => {
+	return res.render ("home", {pageTitle : "Home", videos })
+}
 
 export const see = (req, res) => {
-  const {id} = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", {pageTitle:`Watching ${video.title}`});
-}
+	// req.params.id 값을 id 변수에 할당
+	const {id} = req.params;
+	const video = videos[id -1];
+	return res.render("watch", {pageTitle: `Watching ${video.title}`})
+};
 
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const upload = (req, res) => res.send("Upload")
+export const deleteVideo = (req, res) => res.send("Delete Video")
