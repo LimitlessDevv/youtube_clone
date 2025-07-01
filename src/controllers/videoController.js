f7685bbe90456b090c8071553a3810b2c5532dd0
inyoung <inyoungchoi26@gmail.com>
2025-07-01 15:03:31 +0900

    Edit video part

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: "2 minutes ago",
-    views: 59,
+    views: 1,
     id: 1,
   },
   {
@@ -26,17 +26,21 @@ let videos = [
 ];
 
 export const trending = (req, res) => {
-	return res.render ("home", {pageTitle : "Home", videos })
-}
-
-export const see = (req, res) => {
-	// req.params.id 값을 id 변수에 할당
-	const {id} = req.params;
-	const video = videos[id -1];
-	return res.render("watch", {pageTitle: `Watching ${video.title}`})
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload")
-export const deleteVideo = (req, res) => res.send("Delete Video")
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const {id} = req.params;
+  const {title} = req.body;
+  videos[id-1].title = title;
+  return res.redirect(`/videos/${id}`)
+};
